c30ccd5dd8670d489edbd5bd2142a3fe975c4c27
Andrew Allbright <aallbrig@gmail.com>
2021-02-02 15:00:19 -0500

    Learn about custom editors

---
+++ Assets/Scripts/Editor/ColorRefEditor.cs
@@ -0,0 +1,19 @@
+﻿using ScriptableObjects.Refs;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+  [CustomEditor(typeof(ColorRef))]
+  public class ColorRefEditor : UnityEditor.Editor
+  {
+    private ColorRef colorRef;
+    // public override void OnInspectorGUI()
+    // {
+    //   if (GUILayout.Button("Test Button"))
+    //   {
+    //     Debug.Log("Test button clicked");
+    //   }
+    // }
+  }
+}
---
+++ Assets/Scripts/Editor/FloatRefEditor.cs
@@ -0,0 +1,15 @@
+﻿using ScriptableObjects.Refs;
+using UnityEditor;
+
+namespace Editor
+{
+  [CustomEditor(typeof(FloatRef))]
+  public class FloatRefEditor : UnityEditor.Editor
+  {
+    public override void OnInspectorGUI()
+    {
+      
+      // base.OnInspectorGUI();
+    }
+  }
+}
